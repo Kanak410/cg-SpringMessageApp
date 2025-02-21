fb55940da35cca9c08cea38966ff4c140f9131c9
Kanak Rajput <rajputkanak078@gmail.com>
2025-02-21 23:36:07 +0530

    [kanak]Add . Added path variable to /hello/param end point for personalised greeting

--- src/main/java/com/bridgelabz/kanak/springmessageapp/controller/HelloController.java
+++ src/main/java/com/bridgelabz/kanak/springmessageapp/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.kanak.springmessageapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,12 @@ public class HelloController {
     }
     @GetMapping("/hello/query")
     public String sayHello(@RequestParam(name = "name", defaultValue = "Guest") String name){
+        return "Hello "+name+" from BridgeLabz";
+    }
+
+    //passing name as path variable
+    @GetMapping("hello/param/{name}")
+    public String sayHelloUsingPathVariable(@PathVariable String name){
         return "Hello "+name+" from BridgeLabz";
     }
 
