247da652b76f3b647c1f8e1d040fdaa4a97bd171
Kanak Rajput <rajputkanak078@gmail.com>
2025-02-21 23:05:17 +0530

    [kanak]inital commit

---
+++ src/test/java/com/bridgelabz/kanak/springmessageapp/SpringMessageAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.kanak.springmessageapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMessageAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
