1ab073cf03f028abc7b0d5c7dd0368656fc5b76c
Kanak Rajput <rajputkanak078@gmail.com>
2025-02-21 23:26:42 +0530

    [kanak]Add . Added REST API End point to greet user with name as query parameter

--- src/main/java/com/bridgelabz/kanak/springmessageapp/controller/HelloController.java
+++ src/main/java/com/bridgelabz/kanak/springmessageapp/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.kanak.springmessageapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,6 +9,10 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello(){
         return "Hello ";
+    }
+    @GetMapping("/hello/query")
+    public String sayHello(@RequestParam(name = "name", defaultValue = "Guest") String name){
+        return "Hello "+name+" from BridgeLabz";
     }
 
 }
